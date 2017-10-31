0870376563be879dbb941f004f247595cfb646e4
dorofiy_kolya <dorofiykolya@gmail.com>
2017-10-31 20:47:11 +0200

    first commit

---
+++ Assets/TextureInfo/Editor/TextureChannelEditor.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.IO;
+
+namespace Dorofiy.TextureInfo
+{
+  public class TextureChannelEditor : EditorWindow
+  {
+    [MenuItem("Assets/Texture/Channel")]
+    private static void ShowChannel()
+    {
+      if (ShowChannelValidate())
+      {
+        var texture2d = Selection.activeObject as Texture2D;
+        var path = AssetDatabase.GetAssetPath(texture2d);
+
+        GetWindow<TextureChannelEditor>("Channel").SetData(path, texture2d).Show(true);
+      }
+    }
+
+    [MenuItem("Assets/Texture/Channel", true)]
+    private static bool ShowChannelValidate()
+    {
+      var texture2d = Selection.activeObject as Texture2D;
+      return texture2d != null;
+    }
+
+    private TextureChannelEditor SetData(string path, Texture2D texture2d)
+    {
+      _texture2d = new Texture2D(texture2d.width, texture2d.height);
+      _texture2d.LoadImage(File.ReadAllBytes(path));
+      if (_renderTexture != null)
+      {
+        DestroyImmediate(_renderTexture, true);
+      }
+      _renderTexture = null;
+      return this;
+    }
+
+    private int _selection;
+    private Texture2D _texture2d;
+    private string[] _buttons = new string[] { "rgb", "r", "g", "b", "a" };
+    private Vector2 _scrollPosition;
+    private Texture2D _renderTexture;
+
+    private void OnGUI()
+    {
+      EditorGUILayout.BeginHorizontal();
+      EditorGUILayout.EndHorizontal();
+      var newSelection = GUILayout.SelectionGrid(_selection, _buttons, 5, EditorStyles.miniButtonMid);
+      if (newSelection != _selection)
+      {
+        _selection = newSelection;
+        if (_renderTexture != null)
+        {
+          DestroyImmediate(_renderTexture, true);
+        }
+        _renderTexture = null;
+      }
+
+      if (_renderTexture == null)
+      {
+        _renderTexture = new Texture2D(_texture2d.width, _texture2d.height);
+        for (var x = 0; x < _texture2d.width; x++)
+        {
+          for (var y = 0; y < _texture2d.height; y++)
+          {
+            var pixel = _texture2d.GetPixel(x, y);
+            if (_selection == 0)
+            {
+              _renderTexture.SetPixel(x, y, pixel);
+            }
+            else if (_selection == 1)
+            {
+              pixel.b = pixel.r;
+              pixel.g = pixel.r;
+              pixel.a = 1;
+            }
+            else if (_selection == 2)
+            {
+              pixel.r = pixel.g;
+              pixel.b = pixel.g;
+              pixel.a = 1;
+            }
+            else if (_selection == 3)
+            {
+              pixel.r = pixel.b;
+              pixel.g = pixel.b;
+              pixel.a = 1;
+            }
+            else if (_selection == 4)
+            {
+              pixel.r = pixel.a;
+              pixel.g = pixel.a;
+              pixel.b = pixel.a;
+              pixel.a = 1;
+            }
+            _renderTexture.SetPixel(x, y, pixel);
+          }
+        }
+        _renderTexture.Apply();
+      }
+
+      _scrollPosition = EditorGUILayout.BeginScrollView(_scrollPosition);
+
+      var rect = EditorGUILayout.GetControlRect(GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true));
+      EditorGUI.DrawTextureTransparent(rect, _renderTexture, ScaleMode.ScaleToFit);
+
+      EditorGUILayout.EndScrollView();
+    }
+  }
+}
---
+++ Assets/TextureInfo/Editor/TextureHistogramEditor.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Dorofiy.TextureInfo
+{
+  public class TextureHistogramEditor : EditorWindow
+  {
+    [MenuItem("Assets/Texture/Histogram")]
+    private static void ShowHistogram()
+    {
+      if (ShowHistogramValidation())
+      {
+        var texture2d = Selection.activeObject as Texture2D;
+        var path = AssetDatabase.GetAssetPath(texture2d);
+
+        GetWindow<TextureHistogramEditor>("Histogram").SetData(path, texture2d).Show(true);
+      }
+    }
+
+    [MenuItem("Assets/Texture/Histogram", true)]
+    private static bool ShowHistogramValidation()
+    {
+      var texture2d = Selection.activeObject as Texture2D;
+      return texture2d != null;
+    }
+
+
+    private TextureHistogramEditor SetData(string path, Texture2D texture2d)
+    {
+      Texture2D tex = new Texture2D(texture2d.width, texture2d.height);
+      tex.LoadImage(File.ReadAllBytes(path));
+
+      _pixels = tex.GetPixels32();
+      _histogramData = CalculateHistogram(_pixels, 256);
+      _histogramTexture = null;
+
+      DestroyImmediate(tex, true);
+
+      //minSize = new Vector2(263, 300);
+      //maxSize = new Vector2(263, 300);
+
+      return this;
+    }
+
+    private Texture2D[] _histogramTexture;
+    private int _log10;
+    private int _selection;
+    private HistogramRawData _histogramData;
+    private Color32[] _pixels;
+    private string[] _buttons = new string[] { "rgb", "r", "g", "b", "a" };
+    private string[] _algorithmButtons = new string[] { "linear", "log10" };
+
+    private void OnGUI()
+    {
+      var newLog10 = GUILayout.SelectionGrid(_log10, _algorithmButtons, 2, EditorStyles.miniButtonMid);
+      if (newLog10 != _log10)
+      {
+        _log10 = newLog10;
+        _histogramData = CalculateHistogram(_pixels, 256);
+        _histogramTexture = null;
+      }
+      _selection = GUILayout.SelectionGrid(_selection, _buttons, 5, EditorStyles.miniButtonMid);
+      var rect = EditorGUILayout.GetControlRect(GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true));
+
+      if ((_histogramTexture == null || _histogramTexture[_selection] == null) && _histogramData != null)
+      {
+        if (_histogramTexture == null) _histogramTexture = new Texture2D[5];
+        _histogramTexture[_selection] = GenerateTexture(256, 256, _histogramData, _selection == 0 || _selection == 1, _selection == 0 || _selection == 2, _selection == 0 || _selection == 3, _selection == 4);
+      }
+      else if (_histogramTexture == null && _histogramData == null)
+      {
+        GUILayout.Label("select texture");
+      }
+      else if (_histogramTexture != null && _histogramTexture[_selection] != null)
+      {
+        EditorGUI.DrawTextureTransparent(rect, _histogramTexture[_selection]);
+      }
+    }
+
+    private HistogramRawData CalculateHistogram(Color32[] pixels, int height)
+    {
+      var length = pixels.Length;
+      var r = new long[256];
+      var g = new long[256];
+      var b = new long[256];
+      var a = new long[256];
+      foreach (var pixel in pixels)
+      {
+        r[pixel.r] = r[pixel.r] + 1;
+        g[pixel.g] = g[pixel.g] + 1;
+        b[pixel.b] = b[pixel.b] + 1;
+        a[pixel.a] = a[pixel.a] + 1;
+      }
+
+      var rTotal = 0.0;
+      var gTotal = 0.0;
+      var bTotal = 0.0;
+      var aTotal = 0.0;
+      for (var i = 0; i < 256; i++)
+      {
+        rTotal = Math.Max(rTotal, r[i]);
+        gTotal = Math.Max(gTotal, g[i]);
+        bTotal = Math.Max(bTotal, b[i]);
+        aTotal = Math.Max(aTotal, a[i]);
+      }
+
+      if (_log10 == 1)
+      {
+        rTotal = Math.Log10(rTotal);
+        gTotal = Math.Log10(gTotal);
+        bTotal = Math.Log10(bTotal);
+        aTotal = Math.Log10(aTotal);
+      }
+
+      var rawData = new HistogramRawData();
+      for (var i = 0; i < 256; i++)
+      {
+        var rd = _log10 == 1 ? Math.Log10(r[i]) : r[i];
+        var gd = _log10 == 1 ? Math.Log10(g[i]) : g[i];
+        var bd = _log10 == 1 ? Math.Log10(b[i]) : b[i];
+        var ad = _log10 == 1 ? Math.Log10(a[i]) : a[i];
+
+        rawData.R[i] = rd / rTotal;
+        rawData.G[i] = gd / gTotal;
+        rawData.B[i] = bd / bTotal;
+        rawData.A[i] = ad / aTotal;
+      }
+
+      return rawData;
+    }
+
+    private Texture2D GenerateTexture(int width, int height, HistogramRawData rawData, bool r, bool g, bool b, bool a)
+    {
+      var texture = new Texture2D(width, height);
+
+      for (var i = 0; i < rawData.R.Length; i++)
+      {
+        for (var y = 0; y < height; y++)
+        {
+          texture.SetPixel(i, y, new Color(.1f, .1f, .1f, 1));
+        }
+
+        if (r)
+        {
+          var size = (int)(rawData.R[i] * height);
+          for (var y = 0; y < size; y++)
+          {
+            var pixel = texture.GetPixel(i, y);
+            pixel.r = 1;
+            pixel.a = 1;
+            texture.SetPixel(i, y, pixel);
+          }
+        }
+        if (g)
+        {
+          var size = (int)(rawData.G[i] * height);
+          for (var y = 0; y < size; y++)
+          {
+            var pixel = texture.GetPixel(i, y);
+            pixel.g = 1;
+            pixel.a = 1;
+            texture.SetPixel(i, y, pixel);
+          }
+        }
+        if (b)
+        {
+          var size = (int)(rawData.B[i] * height);
+          for (var y = 0; y < size; y++)
+          {
+            var pixel = texture.GetPixel(i, y);
+            pixel.b = 1;
+            pixel.a = 1;
+            texture.SetPixel(i, y, pixel);
+          }
+        }
+        if (a)
+        {
+          var size = (int)(rawData.A[i] * height);
+          for (var y = 0; y < size; y++)
+          {
+            var pixel = texture.GetPixel(i, y);
+            pixel.r = pixel.a;
+            pixel.g = pixel.a;
+            pixel.b = pixel.a;
+            pixel.a = 1;
+            texture.SetPixel(i, y, pixel);
+          }
+        }
+      }
+      texture.Apply();
+      return texture;
+    }
+
+    private class HistogramRawData
+    {
+      public double[] R = new double[256];
+      public double[] G = new double[256];
+      public double[] B = new double[256];
+      public double[] A = new double[256];
+    }
+  }
+}
